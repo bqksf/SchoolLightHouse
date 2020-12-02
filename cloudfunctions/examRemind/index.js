5ac625ffc1541db6b3e00f0c75a4e71dc2b52d09
tuip123 <tuip123xuzhenghao@outlook.com>
2020-12-02 09:26:06 +0800

    发送考试提醒和课程提醒

--- cloudfunctions/examRemind/index.js
+++ cloudfunctions/examRemind/index.js
@@ -7,7 +7,6 @@ cloud.init({
 const db = cloud.database()
 // 云函数入口函数
 exports.main = async (event, context) => {
-  //TODO! 2.发送模板提醒 3.定时触发器
   var d = new Date()
   var remindTime = d.getHours() + 1
   //获取所有需要提醒的内容
@@ -17,46 +16,40 @@ exports.main = async (event, context) => {
     let exam=examRemindList[e]
     //判断时间
     if (exam.examtime.split(':')[0] == remindTime) {
-      console.log("TODO:发送提醒消息")
+      await cloud.openapi.uniformMessage.send({
+        touser: exam._openidGZH,
+        mpTemplateMsg: {
+          appid: 'wx3df92dead7bcd174',
+          templateId: 'vaiyMl11zeD9l-nhBXPhxrZ2sbv9aD4Hb6ePed59ZT8',
+          url: '',
+          data: {
+            "first": {
+              "value": "考试即将开始",
+              "color": "#173177"
+            },
+            "keyword1": {
+              "value": exam.examName,
+              "color": "#173177"
+            },
+            "keyword2": {
+              "value": exam.examtime,
+              "color": "#173177"
+            },
+            "remark": {
+              "value": "请注意带好纸笔，不要迟到",
+              "color": "#173177"
+            }
+          },
+          miniprogram:{
+            appid:'wxf203d0e6cfbed41a',
+            page:'pages/index?foo=bar'
+          }
+        }
+      })
       //用完就删
       await db.collection("examRemindList").where({
         _id:exam._id
       }).remove()
     }
   }
-  // await got.post('https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=ACCESS_TOKEN',{
-  //   json:{
-  //     //TODO 公众号的openid
-  //     "touser": "",
-  //     //TODO 模板id
-  //     "template_id": "",
-  //     //TODO 跳转页面
-  //     "url": "",
-  //     "topcolor": "#FF0000",
-  //     //TODO 数据，event中或者数据库中提供
-  //     "data": {
-  //       "first": {
-  //         "value": "tuip123",
-  //         "color": "#173177"
-  //       },
-  //       "keyword1": {
-  //         "value": "tuip123-kw1",
-  //         "color": "#173177"
-  //       },
-  //       "keyword2": {
-  //         "value": "tuip123-kw2",
-  //         "color": "#173177"
-  //       },
-  //       "keyword3": {
-  //         "value": "tuip123-kw3",
-  //         "color": "#173177"
-  //       },
-  //       "remark": {
-  //         "value": "tuipcain",
-  //         "color": "#173177"
-  //       }
-  //     }
-  //   },
-  //   responseType:'json'
-  // })
 }
--- cloudfunctions/scheduleRemind/index.js
+++ cloudfunctions/scheduleRemind/index.js
@@ -15,6 +15,36 @@ exports.main = async (event, context) => {
     let schedule = scheduleRemindList[s]
     if (schedule.time.split(':')[0] == remindTime) {
       console.log("TODO:发送提醒消息")
+      await cloud.openapi.uniformMessage.send({
+        touser:schedule._openidGZH,
+        mpTemplateMsg: {
+          appid: 'wx3df92dead7bcd174',
+          templateId: '-YwXVzr-AmtEGt0vXElhzIfDJRFuOqdSreDI0Og-Rg4',
+          url: '',
+          data:{
+            "first": {
+              "value": "课程即将开始",
+              "color": "#173177"
+            },
+            "keyword1": {
+              "value": schedule.name,
+              "color": "#173177"
+            },
+            "keyword2": {
+              "value": schedule.time,
+              "color": "#173177"
+            },
+            "remark": {
+              "value": "上课地点："+schedule.place+"不要迟到",
+              "color": "#173177"
+            }
+          },
+          miniprogram:{
+            appid:'wxf203d0e6cfbed41a',
+            page:'pages/index?foo=bar'
+          }
+        }
+      })
       //用完就删
       await db.collection("scheduleRemindList").where({
         _id:schedule._id
